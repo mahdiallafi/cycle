7d8e132d1493d800b13edf87965c5fcbe3e4ead8
tim1811 <timalexander.1811@gmail.com>
2023-12-09 17:02:12 +0200

    BetterExamples

--- cycle1/timStuff/test.py
+++ cycle1/timStuff/test.py
@@ -10,3 +10,7 @@ print(Q[10, 11, 99 , 79])
 
 userData = [1,1,1,1,1,1,1,1,1,1] # How to get the form data into this encoding we will do later.
 print(getBestGreedy(None, 15000, 90000, userData, "St. Peter’s Church"))
+print(getBestGreedy(None, 15000, 90000, userData, None))
+print(getBestGreedy(None, 15000, 90000, userData, None))
+print(getBestGreedy("Volksparkstadion", 15000, 90000, userData, "St. Peter’s Church"))
+# None as starting can lead to "empty" routes as place can be randomly selected that isn't part of the routematrix yet
