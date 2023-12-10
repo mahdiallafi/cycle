c7eb4f952f90256e005f29c37425a28e5ce5332e
mahdiallafi <mahdiallafi1@gmail.com>
2023-12-10 18:06:59 +0200

    Show name,address.description,website

--- cycle1/knn_script.py
+++ cycle1/knn_script.py
@@ -17,10 +17,10 @@ def run_knn(age, gender, maxDestination, minDestination, history, art, nature, s
 
     # Make a prediction using form data
     similar_items = [
-        {'locations': 'Brandenburg Gate', 'address': 'Sylvesterallee 7, 22525 Hamburg, Germany', 'description': 'test', 'website': 'https://example.com/brandenburg_gate'},
-        {'locations': 'Berlin Cathedral', 'address': 'Harald-Stender-Platz 1, 20359 Hamburg, Germany', 'description': 'https://example.com/berlin_cathedral_image.jpg', 'website': 'https://example.com/berlin_cathedral'},
-        {'locations': 'Checkpoint Charlie', 'address': 'Jakobikirchhof 22, 20095 Hamburg, Germany', 'description': 'https://example.com/checkpoint_charlie_image.jpg', 'website': 'https://example.com/checkpoint_charlie'},
-        {'locations': 'East Side Gallery', 'address': 'Mönckebergstraße, 20095 Hamburg, Germany', 'description': 'https://example.com/east_side_gallery_image.jpg', 'website': 'https://example.com/east_side_gallery'},
+        {'locations': 'Brandenburg Gate','name':'Sylvesterallee', 'address': 'Sylvesterallee 7, 22525 Hamburg, Germany', 'description': 'test', 'website': 'https://example.com/brandenburg_gate'},
+        {'locations': 'Berlin Cathedral','name':'Harald-Stender-Platz', 'address': 'Harald-Stender-Platz 1, 20359 Hamburg, Germany', 'description': 'https://example.com/berlin_cathedral_image.jpg', 'website': 'https://example.com/berlin_cathedral'},
+        {'locations': 'Checkpoint Charlie','name':'Jakobikirchhof', 'address': 'Jakobikirchhof 22, 20095 Hamburg, Germany', 'description': 'https://example.com/checkpoint_charlie_image.jpg', 'website': 'https://example.com/checkpoint_charlie'},
+        {'locations': 'East Side Gallery','name':'Mönckebergstraße', 'address': 'Mönckebergstraße, 20095 Hamburg, Germany', 'description': 'https://example.com/east_side_gallery_image.jpg', 'website': 'https://example.com/east_side_gallery'},
     ]
 
     return similar_items
